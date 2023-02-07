4cb71ac0500726adf959bed0c603b4c887bc6f7a
lomlomm <vanessaserna60@gmail.com>
2023-02-07 11:39:40 -0600

    Solved calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return(a+b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return(a-b)
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+  let suma = 0;
+	for (let value in values){
+    console.log(values[value])
+    suma += values[value]
+  }
+  return(suma)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  result = 1
+  for(let number in numbers){
+    result *= numbers[number]
+  }
+  return(result)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return(a**b)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorial = 1
+  for(let i=0; i<number; i++){
+    factorial *= i+1
+  } 
+  return(factorial)
 };
 
 // Do not edit below this line
